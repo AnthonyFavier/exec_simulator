1fa9714cf98639852917a3957b193676150ff0b9
AnthonyFavier <anthony.favier@laas.fr>
2023-09-13 11:06:32 +0200

    ompl planning dua RRT* SBL

--- src/tiago/tiago_tutorials/tiago_moveit_tutorial/src/own_plan_arm_torso_ik.cpp
+++ src/tiago/tiago_tutorials/tiago_moveit_tutorial/src/own_plan_arm_torso_ik.cpp
@@ -48,7 +48,8 @@
 #include <string>
 #include <vector>
 
-moveit::planning_interface::MoveGroupInterface* group_arm_torso;
+moveit::planning_interface::MoveGroupInterface* group_arm_torso_opti;
+moveit::planning_interface::MoveGroupInterface* group_arm_torso_fast;
 ros::Publisher start_moving_pub;
 std::vector<std::string> named_targets;
 std_msgs::Empty e_msg;
@@ -63,19 +64,30 @@ bool move_named_target_server(sim_msgs::MoveArmRequest &req, sim_msgs::MoveArmRe
 	else
 	{
 		// Set goal pose
-		group_arm_torso->setNamedTarget(req.named_target);
+		group_arm_torso_opti->setNamedTarget(req.named_target);
+		group_arm_torso_fast->setNamedTarget(req.named_target);
 
     // Plan
     moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-    bool success = bool(group_arm_torso->plan(my_plan));
+    bool success = bool(group_arm_torso_opti->plan(my_plan));
+    bool fast = false;
+    if ( !success )
+    {
+      fast = true;
+      ROS_WARN("Opti plan not found, planning with fast solver.");
+      success = bool(group_arm_torso_fast->plan(my_plan));
+    }
     if ( !success )
       throw std::runtime_error("No plan found");
+    double time = my_plan.planning_time_;
+    if(fast)
+      time += group_arm_torso_opti->getPlanningTime();
     ROS_INFO_STREAM("Plan found in " << my_plan.planning_time_ << " seconds");
 
     // Execute the plan
     ros::Time start = ros::Time::now();
     start_moving_pub.publish(e_msg);
-    moveit::planning_interface::MoveItErrorCode e = group_arm_torso->move();
+    moveit::planning_interface::MoveItErrorCode e = group_arm_torso_opti->execute(my_plan);
     if (!bool(e))
       throw std::runtime_error("Error executing plan");
     ROS_INFO_STREAM("Motion duration: " << (ros::Time::now() - start).toSec());
@@ -89,7 +101,8 @@ bool move_pose_target_server(sim_msgs::MoveArmRequest &req, sim_msgs::MoveArmRes
 	ROS_INFO("Pose target received.");
 
   // Set start state
-  group_arm_torso->setStartStateToCurrentState();
+  group_arm_torso_opti->setStartStateToCurrentState();
+  group_arm_torso_fast->setStartStateToCurrentState();
 
 	// Set goal pose
   geometry_msgs::PoseStamped goal_pose;
@@ -99,19 +112,30 @@ bool move_pose_target_server(sim_msgs::MoveArmRequest &req, sim_msgs::MoveArmRes
   goal_pose.pose.orientation.y = 0;
   goal_pose.pose.orientation.z = 0;
   goal_pose.pose.orientation.w = 1;
-	group_arm_torso->setPoseTarget(req.pose_target);
+	group_arm_torso_opti->setPoseTarget(req.pose_target);
+	group_arm_torso_fast->setPoseTarget(req.pose_target);
 
 	// Plan
   moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-  bool success = bool(group_arm_torso->plan(my_plan));
+  bool success = bool(group_arm_torso_opti->plan(my_plan));
+    bool fast = false;
+  if ( !success )
+  {
+      fast = true;
+      ROS_WARN("Opti plan not found, planning with fast solver.");
+      success = bool(group_arm_torso_fast->plan(my_plan));
+  }
   if ( !success )
     throw std::runtime_error("No plan found");
+  double time = my_plan.planning_time_;
+  if(fast)
+    time += group_arm_torso_opti->getPlanningTime();
   ROS_INFO_STREAM("Plan found in " << my_plan.planning_time_ << " seconds");
 
 	// Execute the plan
   ros::Time start = ros::Time::now();
   start_moving_pub.publish(e_msg);
-  moveit::planning_interface::MoveItErrorCode e = group_arm_torso->move();
+  moveit::planning_interface::MoveItErrorCode e = group_arm_torso_opti->execute(my_plan);
   if (!bool(e))
     throw std::runtime_error("Error executing plan");
   ROS_INFO_STREAM("Motion duration: " << (ros::Time::now() - start).toSec());
@@ -130,18 +154,23 @@ int main(int argc, char** argv)
   spinner.start();
 
   //select group of joints
-  group_arm_torso = new moveit::planning_interface::MoveGroupInterface("arm_torso");
-  // group_arm_torso = new moveit::planning_interface::MoveGroupInterface("arm");
+  group_arm_torso_opti = new moveit::planning_interface::MoveGroupInterface("arm_torso");
+  group_arm_torso_fast = new moveit::planning_interface::MoveGroupInterface("arm_torso");
   //choose your preferred planner
-  group_arm_torso->setPlannerId("SBLkConfigDefault");
-  // group_arm_torso->setPlannerId("RRTConnectkConfigDefault");
-  group_arm_torso->setPoseReferenceFrame("world"); 
-  group_arm_torso->setMaxVelocityScalingFactor(1.0);
-	group_arm_torso->setMaxAccelerationScalingFactor(0.1);
-  group_arm_torso->setPlanningTime(5.0);
-	group_arm_torso->setGoalOrientationTolerance(10.0);
-	group_arm_torso->setGoalPositionTolerance(0.01);
-	named_targets = group_arm_torso->getNamedTargets();
+  group_arm_torso_opti->setPlannerId("RRTstarkConfigDefault");
+  group_arm_torso_opti->setPoseReferenceFrame("world"); 
+  group_arm_torso_opti->setMaxVelocityScalingFactor(1.0);
+	group_arm_torso_opti->setMaxAccelerationScalingFactor(0.2);
+  group_arm_torso_opti->setPlanningTime(0.5);
+	group_arm_torso_opti->setGoalOrientationTolerance(10.0);
+	group_arm_torso_opti->setGoalPositionTolerance(0.01);
+
+  group_arm_torso_fast->setPlannerId("SBLkConfigDefault");
+  group_arm_torso_fast->setPoseReferenceFrame("world"); 
+	group_arm_torso_fast->setGoalOrientationTolerance(10.0);
+	group_arm_torso_fast->setGoalPositionTolerance(0.01);
+
+	named_targets = group_arm_torso_opti->getNamedTargets();
 
   start_moving_pub = nh.advertise<std_msgs::Empty>("/r_start_moving", 1);
 	ros::ServiceServer move_pose_target_service = nh.advertiseService("move_pose_target", move_pose_target_server);
@@ -151,7 +180,7 @@ int main(int argc, char** argv)
   while(ros::ok())
     loop.sleep();
 
-  delete group_arm_torso;
+  delete group_arm_torso_opti;
   spinner.stop();
   return EXIT_SUCCESS;
 }
