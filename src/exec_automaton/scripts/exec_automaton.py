274ad7a51b7d0dcf27dc63f8b2a955440b0b86a5
AnthonyFavier <anthony.favier@laas.fr>
2023-10-17 09:49:33 +0200

    all sols

--- src/exec_automaton/scripts/exec_automaton.py
+++ src/exec_automaton/scripts/exec_automaton.py
@@ -1322,9 +1322,9 @@ def main_exec():
     sol_tt_tee =    None
     sol_tt_hmw =    None
     sol_tee =       load("sol_stack_empiler_2_tee.p")
-    # sol_hmw =       load("sol_stack_empiler_2_hmw.p")
+    sol_hmw =       load("sol_stack_empiler_2_hmw.p")
     sol_tt_tee =    load("sol_stack_empiler_2_tt_tee.p")
-    # sol_tt_hmw =    load("sol_stack_empiler_2_tt_hmw.p")
+    sol_tt_hmw =    load("sol_stack_empiler_2_tt_hmw.p")
     if g_domain_name!=DOMAIN_NAME:
         raise Exception("Missmatching domain names CONSTANT and loaded")
     robots = {
