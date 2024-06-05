25da15d61fcef9f45870628d2942847eb2d70f64
AnthonyFavier <anthony.favier@laas.fr>
2024-06-05 16:32:38 +0200

    minor script launch

--- scripts/full_start.sh
+++ scripts/full_start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-export GAZEBO_PLUGIN_PATH=`rospack find exec_automaton`/../gazebo_plugin/build
+
 # window prompt
 sh `rospack find exec_automaton`/../../scripts/prompt_window.sh
+
+# Simulator
+export GAZEBO_PLUGIN_PATH=`rospack find exec_automaton`/../gazebo_plugin/build
 roslaunch tiago_gazebo tiago_gazebo.launch
--- scripts/simu_start.sh
+++ scripts/simu_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+
 export GAZEBO_PLUGIN_PATH=`rospack find exec_automaton`/../gazebo_plugin/build
 roslaunch tiago_gazebo tiago_gazebo.launch
