fdc2195da257e262b165b9f2590dc9dd66ff9b1a
AnthonyFavier <anthony.favier@laas.fr>
2024-06-20 13:41:35 +0200

    save frame Fix mkdir + rm prints

--- src/simulator/scripts/saving_bird_camera.py
+++ src/simulator/scripts/saving_bird_camera.py
@@ -4,6 +4,7 @@ import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
+import os
 
 i=0
 
@@ -13,12 +14,14 @@ def image_callback(msg):
         cv_image = bridge.imgmsg_to_cv2(msg, "bgr8")
         filename = f"/tmp/camera_save_tutorial/frame_{i}.jpg"
         cv2.imwrite(filename, cv_image)
-        rospy.loginfo(f"Saved frame at {filename}")
+        # rospy.loginfo(f"Saved frame at {filename}")
         i+=1
     except CvBridgeError as e:
         rospy.logerr(f"Could not convert image: {e}")
 
 if __name__ == '__main__':
+    os.mkdir("/tmp/camera_save_tutorial")
+
     rospy.init_node('camera_listener', anonymous=True)
     bridge = CvBridge()
     rospy.Subscriber("/bird_camera/rgb/image_raw", Image, image_callback, queue_size=1)
