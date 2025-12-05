d9cc55a70cf3252b66ecf5562efb63506765a7df
AnthonyFavier <antfav24@mit.edu>
2025-12-05 14:36:44 -0500

    prompt window script commented

--- scripts/full_start.sh
+++ scripts/full_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export GAZEBO_PLUGIN_PATH=`rospack find exec_automaton`/../gazebo_plugin/build
 # window prompt
-sh `rospack find exec_automaton`/../../scripts/prompt_window.sh
+# sh `rospack find exec_automaton`/../../scripts/prompt_window.sh
 roslaunch tiago_gazebo tiago_gazebo.launch
--- scripts/prompt_window.sh
+++ scripts/prompt_window.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-terminator -b -p simulation_prompt --geometry '710x250+1210+0'
+
+# With specific Terminator profile named 'simulation_prompt'
+    # Bigger font, no scroll bar, no cursor blinking, no title bar
+# terminator -b -p simulation_prompt --geometry '710x250+1210+0'
+
+# Without specific Terminator profile
+terminator -b --geometry '710x250+1210+0'
